04e7686e7cc457c7da25cc48cc0e917a87b1ce05
Naton <anton.migel.99@mail.ru>
2020-03-16 21:25:12 +0300

    Add test

---
+++ LineTest/Linetest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DEV_1._1;
+
+namespace LineTest
+{
+    [TestClass]
+    public class LineTest
+    {
+        [TestMethod]
+        public void testGetNumberDiffSymbol()
+        {
+            int expected = 3;
+            Line line = new Line("aba");
+            Assert.AreEqual(line.getCountDiffSymbol(), expected);
+        }
+        [TestMethod]
+        public void testNUllValue()
+        {
+            int expected =0;
+            Line line = new Line(null);
+            Assert.AreEqual(line.getCountDiffSymbol(), expected);
+        }
+        [TestMethod]
+        public void testEmptyValue()    
+        {
+            int expected = 0;
+            Line line = new Line(String.Empty);
+            Assert.AreEqual(line.getCountDiffSymbol(), expected);
+        }
+    }
+}
