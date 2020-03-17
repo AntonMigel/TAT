08492b162277eaf0d80dd1e96a7db010a85b48de
Naton <anton.migel.99@mail.ru>
2020-03-17 14:51:16 +0300

    add method convert numbers

--- Converter/Converter/Converter.cs
+++ Converter/Converter/Converter.cs
@@ -8,5 +8,31 @@ namespace Converter
 {
     class Converter
     {
+        /// <summary>
+        /// Convert number to various systems(2-20)
+        /// </summary>
+        /// <returns>result</returns>
+        public static string convertVariousSystem(string number, string Radix )
+        {
+            const int BitsInIng = 32;
+            const string Digits = "0123456789ABCDEFGHIJ";
+            int decimalNumber = Convert.ToInt32(number);
+            int radix = Convert.ToInt32(Radix);
+
+            if (decimalNumber == 0)
+                return "0";
+            int index = BitsInIng - 1;
+            char[] charArray = new char[BitsInIng];
+
+            while (decimalNumber != 0)
+            {
+                int remainder = decimalNumber % radix;
+                charArray[index--] = Digits[remainder];
+                decimalNumber = decimalNumber / radix;
+            }
+
+            string result = new String(charArray, index + 1, BitsInIng - index - 1);
+            return result;
+        }
     }
 }
