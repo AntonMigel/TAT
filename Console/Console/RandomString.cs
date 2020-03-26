2962695652f36304b239e275ab845f0f0e2a1b37
Naton <anton.migel.99@mail.ru>
2020-03-26 22:34:40 +0300

    add task

---
+++ Console/Console/RandomString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentEvent
+{
+    class  RandomString
+    {
+        public static string getRandomString() 
+        {
+            string symbols = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int length = 7;
+            var rand = new Random();
+            StringBuilder builder = new StringBuilder(length);
+
+            for (int i = 0; i < length; ++i) 
+            {
+                builder.Append(symbols[rand.Next(symbols.Length)]); 
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Console/Console/TaskResults.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentEvent
+{
+   
+        struct TaskResults
+        {
+
+            public string Name;
+            public string Surname;
+            public string Answer;
+            public TaskResults(string name, string surname, string task)
+            {
+                Name = name;
+                Surname = surname;
+                Answer = task;
+            }
+        }
+}
