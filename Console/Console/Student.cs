461016a2c85739ff007f098b86cefdaa45a92702
Naton <anton.migel.99@mail.ru>
2020-03-26 22:56:27 +0300

    Add gitignore and task

---
+++ Console/Console/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentEvent
+{
+    class Student : ITaskPerformer
+    {
+        private string _surname;
+        private string _name;
+
+        public delegate void StudentHandler(Student student,string str);
+        public event StudentHandler Handler;
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public Student(string name, string surname) 
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public void DoTask() 
+        {
+            string result = RandomString.getRandomString();
+            Handler?.Invoke(this, result);
+        }
+
+    }
+}
