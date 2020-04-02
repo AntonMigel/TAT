807b25fd7c3cd2634246b47995fad3724b1f36fb
AntonMigel <61519779+AntonMigel@users.noreply.github.com>
2020-04-02 20:30:25 +0300

    Update Program.cs

--- Converter/Converter/Program.cs
+++ Converter/Converter/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Converter
+using System;
+namespace Converter
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+          try
+            {
+                Console.WriteLine("Enter a number :");
+                string number = Console.ReadLine();
+                Console.WriteLine("Enter a radix :");
+                string radix = Console.ReadLine();
+                Console.WriteLine(сonvertFromDec.ConvertVariousSystem(number, radix));
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine($"Ошибка: {exp.Message}");
+            }
         }
     }
 }
