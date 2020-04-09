391192a473f7c3fc15cc60d63c52fdcc5b1ef110
Naton <anton.migel.99@mail.ru>
2020-04-09 14:37:07 +0300

    add gitignore and some files

---
+++ task_DEV-2.1/task_DEV-2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_DEV_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
