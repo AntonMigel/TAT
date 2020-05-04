47999cea84a3a9ccfcde3b25620723d6e18cb69e
Naton <anton.migel.99@mail.ru>
2020-05-04 14:12:27 +0300

    Start project

---
+++ task_DEV-2.3/task_DEV-2.3/Message.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace task_DEV_2._3
+{
+    class Message
+    {
+        string WRITEMESSAGEBUTTON = "";
+        string WHOMESSAGE = "";
+        string TEXTMESSAGE = "";
+        string BUTTONSEND = "";
+
+        const string INPUT_TEXT = "HELLO";
+        IWebDriver _driver;
+        
+        public Message(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        
+        public Message WriteMessage(string email)
+        {
+           _driver.FindElement(By.XPath(WRITEMESSAGEBUTTON)).Click();
+           _driver.FindElement(By.XPath(WHOMESSAGE)).SendKeys(email);
+           _driver.FindElement(By.XPath(TEXTMESSAGE)).SendKeys(INPUT_TEXT);
+           _driver.FindElement(By.XPath(BUTTONSEND)).Click();
+            return this;
+        }
+    }
+}
---
+++ task_DEV-2.3/task_DEV-2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace task_DEV_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://mail.ru/";
+            Login log = new Login(driver);
+            log.LogIN("tattovich", "uA7pA3oPTah[");
+            Message message = new Message(driver);
+            message.WriteMessage("");
+        }
+    }
+}
